5f2f4d4e588e3e23896848167ee5241d93bd9c87
Seven Lju <dna2gle@gmail.com>
2024-04-01 21:24:00 +0800

    agricola: add action stat check

--- agricola/player.js
+++ agricola/player.js
@@ -50,10 +50,20 @@ function player_add_occupation(player, lesson) {
 function player_build_effects(deck, player) {
    // TODO: list all possible effects before use a family member to make one action
    //       e.g. baking vegetable to food if having fireplace
+   const effect = [];
+   if (player.applied_major_cards.find(x => x.name == 'fireplace' || x.name == 'cooking_hearth')) {
+      if (player.sheep > 0) effect.push({ transform: ['sheep', 'food'] });
+      if (player.boar > 0) effect.push({ transform: ['boar', 'food'] });
+      if (player.cattle > 0) effect.push({ transform: ['cattle', 'food'] });
+   }
+   if (player.grain > 0) effect.push({ transform: ['grain', 'food'] });
+   if (player.vegetable > 0) effect.push({ transform: ['vegetable', 'food'] });
 }
 function player_build_actions(deck, player) {
    // TODO: according to deck, generate possible effects if take actions
    // effect: { costs, req, ...{benefits+conditions} }
+   const stat = player_check_available_action(player, deck);
+   // TODO: for each action, check available status
 }
 function player_build_fences(player) {
    // TODO: according to count(wood), generate possible solutions
@@ -77,6 +87,91 @@ function player_get_extra_score(player, deck) {
    // TODO: get player score with not-current-player info on a deck
 }
 
+function _player_stat(player) {
+   const r = {
+      room: 0,
+      empty: 0,
+      plowed: 0,
+      empty_plowed: 0,
+      fenced: 0,
+      stable: 0,
+      fenced_stable: 0,
+      min_wood_fence: 9,
+   };
+   player.fields.forEach((row, i) => {
+      row.forEach((cell, j) => {
+         if (cell.room) r.room_n++;
+         if (Object.keys(cell).length == 0) r.empty++;
+         if (cell.plowed) {
+            r.plowed++;
+            if (!cell.sowed) r.empty_plowed++;
+         }
+         if (cell.fenced) {
+            r.fenced++;
+            if (cell.stable) r.fenced_stable++;
+         }
+         if (cell.stable) r.stable++;
+         if (!cell.room && !cell.plowed) {
+            const ib = i*2;
+            const x = (
+               (player.fences[ib][j] == 1 ? 0 : 1) +
+               (player.fences[ib+1][j] == 1 ? 0 : 1) +
+               (player.fences[ib+1][j+1] == 1 ? 0 : 1) +
+               (player.fences[ib+2][j] == 1 ? 0 : 1)
+            );
+            if (r.min_wood_fence > x && x > 0) r.min_wood_fence = x;
+         }
+      });
+   });
+   return r;
+}
+
+const always_available_actions = [
+   'sheep', 'boar', 'cattle', 'wood', 'clay', 'stone', 'reed',
+   'grain', 'vegetable', 'food', 'fisrt_player',
+];
+function player_check_available_action(player, deck) {
+   const map = {};
+   const stat = _player_stat(player);
+   always_available_actions.forEach(one => { map[one] = true; });
+
+   // TODO: occupation + minor improvement reduce
+   const room_n = stat.room;
+   map.renovating = player.reed >= 1 && (
+      player.roomlv == 0 && player.clay >= room_n
+   ) || (
+      player.roomlv == 1 && player.stone >= room_n
+   );
+
+   const min_wood_fences = stat.min_wood_fence;
+   map.fencing = min_wood_fences != 9 && player.wood >= min_wood_fences;
+
+   const empty_n = stat.empty;
+   map.plowing = empty_n > 0;
+
+   const empty_plowed_n = stat.plowed;
+   map.sowing = empty_plowed_n > 0 && (player.grain > 0 || player.vegetable > 0);
+
+   map.baking = player.grain > 0;
+   map.room_building = empty_n > 0 && player.reed >= 2 (
+      (player.roomlv == 0 && player.wood >= 5) ||
+      (player.roomlv == 1 && player.clay >= 5) ||
+      (player.roomlv == 2 && player.stone >= 5) ||
+   );
+   const fenced_stable_n = stat.fenced_stable;
+   const fenced_n = stat.fenced;
+   const stable_n = stat.stable;
+   map.stable_building = player.wood >= 2 && stable_n < 5 && (
+      empty_n > 0 || (fenced_stable_n < fenced_n)
+   );
+   map.childbearing = player.actors < 5 && room_n > player.actors;
+   map.childbearing_urgent = player.actors < 5;
+   map.learning = player.occupations.length > 0;
+   map.minor_improvement = player.minor_cards.length > 0;
+   map.major_improvement = deck ? deck.major_cards.length > 0 : false;
+   return map;
+}
+
 module.exports = {
    player_new,
    player_add_minor_card,
@@ -88,4 +183,5 @@ module.exports = {
    player_build_stables,
    player_get_self_score,
    player_get_extra_score,
+   player_check_available_action,
 };
